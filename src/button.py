56dc413acae7c8e4db359d3b96049dae4fd75c3d
OakmanByte <antonskebaber@gmail.com>
2023-03-01 21:18:18 +0100

    wip

--- src/button.py
+++ src/button.py
@@ -1,38 +1,28 @@
 from dataclasses import dataclass
 import pygame
-from pygame import Surface, SurfaceType
 
 
 @dataclass
 class Button:
-    x: int
-    y: int
-    width: int
-    height: int
+    x: int = 0
+    y: int = 0
+    width: int = 64
+    height: int = 64
     color: tuple[int, int, int] = (0, 0, 0)
-    icon_path: str = None
-    icon: Surface | SurfaceType = None
-
-    def __post_init__(self):
-        if self.icon_path:
-            self.icon = pygame.image.load(self.icon_path)
 
     def is_over(self, mouse_position: tuple[int, int]):
         return self.x <= mouse_position[0] <= (self.x + self.width) \
                and self.y <= mouse_position[1] <= (self.y + self.height)
 
-    def draw(self, screen):
-        if self.icon:
-            screen.blit(self.icon, (self.x, self.y))
-        else:
-            pygame.draw.rect(screen, color=self.color, rect=(self.x, self.y, self.width, self.height))
+    def draw(self, screen, outline: bool = False):
+        pygame.draw.rect(screen, color=self.color, rect=(self.x, self.y, self.width, self.height))
+        if outline:
+            pygame.draw.rect(screen, color=(0, 0, 0), rect=(self.x, self.y, self.width, self.height), width=1)
 
+    def set_pos(self, x, y):
+        self.x = x
+        self.y = y
 
-@dataclass
-class Bomb(Button):
-
-    def __post_init__(self):
-        self.icon = pygame.image.load("gift.svg")
-
-    def on_click(self):
-        print("boom!")
+    def set_dimensions(self, height, width):
+        self.height = height
+        self.width = width
--- src/main.py
+++ src/main.py
@@ -1,64 +1,81 @@
+import math
+
 import pygame
 
-from button import Button, Bomb
+from button import Button
 import random
 
-WINDOW_HEIGHT = 900
-WINDOW_WIDTH = 900
+WINDOW_HEIGHT = 614
+WINDOW_WIDTH = 614
 SCREEN = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 CLOCK = pygame.time.Clock()
+BOARD_HEIGHT = 512
+BOARD_WIDTH = 512
+NUM_OF_BOARD_ITEMS = 64
+OFFSET = round((WINDOW_WIDTH / 2) - (BOARD_WIDTH / 2))
+FRAME_BORDER_COLOR = (128, 128, 128)
+FRAME_BORDER_THICKNESS = 4
 
 
 def main():
     pygame.init()
     SCREEN.fill((0, 0, 0))
+    game_board = populate_board_array()
+    set_board_item_locations(game_board)
+    print(game_board)
 
     while True:
-        game_board = init_board()
-        print(game_board)
-
         for event in pygame.event.get():
+
+            pygame.draw.rect(SCREEN,
+                             color=FRAME_BORDER_COLOR,
+                             rect=(
+                                 OFFSET, OFFSET, BOARD_WIDTH + (FRAME_BORDER_THICKNESS * 2),
+                                 BOARD_HEIGHT + (FRAME_BORDER_THICKNESS * 2)),
+                             width=4)
+
+            for board_item in game_board:
+                board_item.draw(SCREEN, outline=True)
+                if board_item.is_over(pygame.mouse.get_pos()):
+                    print("over a button")
+
             if event.type == pygame.QUIT:
                 pygame.quit()
             if event.type == pygame.MOUSEBUTTONDOWN:
                 mouse_presses = pygame.mouse.get_pressed()
                 if mouse_presses[0]:
                     print("Left Mouse key was clicked")
-                    mouse_position = pygame.mouse.get_pos()
 
         pygame.display.update()
 
 
-def init_board(rows: int = 8, columns: int = 8, button_size: int = 20, spacing: int = 5, number_of_bombs: int = 5):
-
+def populate_board_array(number_of_bombs: int = 5):
     # Generate random location for bombs
     entity_list = [int]
     for i in range(0, number_of_bombs):
-        n = random.randint(0, rows * columns)
+        n = random.randint(0, NUM_OF_BOARD_ITEMS)
         entity_list.append(n)
 
     # Populate board with bombs and normal squares
     board = []
-    for row in range(rows):
-        game_row = []
-        for column in range(columns):
-            if (row + column) in entity_list:
-                game_row.append(Bomb(x + spacing, y + spacing, button_size, button_size))
-            else:
-                game_row.append(Button(x + spacing, y + spacing, button_size, button_size, (129, 0, 128)))
-        board.append(game_row)
+    for row in range(NUM_OF_BOARD_ITEMS):
+        if row in entity_list:
+            board.append(Button(color=(255, 0, 0)))
+        else:
+            board.append(Button(color=(255, 255, 255)))
 
     return board
-    '''
-    button_grid = []
-    for x in range(spacing, WINDOW_WIDTH, button_size + spacing):
-        row = []
-        for y in range(spacing, WINDOW_HEIGHT, button_size + spacing):
-            button = Button(x, y, button_size, button_size, (129, 0, 128))
-            row.append(button)
-        button_grid.append(row)
-    return button_grid
-    '''
+
+
+def set_board_item_locations(game_board: list[Button]):
+    spacing = 5
+    size = BOARD_HEIGHT // math.sqrt(NUM_OF_BOARD_ITEMS)
+    for i in range(NUM_OF_BOARD_ITEMS):
+        row = i // 8
+        col = i % 8
+        x = (col * size) + spacing
+        y = (row * size) + spacing
+        game_board[i].set_pos(x=x + OFFSET, y=y + OFFSET)
 
 
 if __name__ == '__main__':
