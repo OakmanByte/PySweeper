2a909967d239e7b450190d00adfdc16dce92064e
OakmanByte <antonskebaber@gmail.com>
2023-03-02 22:10:24 +0100

    wip

--- src/button.py
+++ src/button.py
@@ -1,14 +1,23 @@
 from dataclasses import dataclass
+from enum import Enum
+from typing import Tuple
 import pygame
 
 
+class BoardItemTypes(Enum):
+    BOMB = "Bomb"
+    NUMBER = "Number"
+
+
 @dataclass
-class Button:
+class BoardItem:
+    type: BoardItemTypes
+    hidden: bool = True
     x: int = 0
     y: int = 0
     width: int = 64
     height: int = 64
-    color: tuple[int, int, int] = (0, 0, 0)
+    color: Tuple[int, int, int] | str = (255, 255, 255)
 
     def is_over(self, mouse_position: tuple[int, int]):
         return self.x <= mouse_position[0] <= (self.x + self.width) \
@@ -26,3 +35,41 @@ class Button:
     def set_dimensions(self, height, width):
         self.height = height
         self.width = width
+
+    def reveal(self):
+        self.hidden = False
+
+
+@dataclass
+class Bomb(BoardItem):
+    type: BoardItemTypes = BoardItemTypes.BOMB
+
+    def draw(self, screen, outline: bool = False):
+        color = "white" if self.hidden else "red"
+        pygame.draw.rect(screen, color=color, rect=(self.x, self.y, self.width, self.height))
+        if outline:
+            pygame.draw.rect(screen, color=(0, 0, 0), rect=(self.x, self.y, self.width, self.height), width=1)
+
+    def reveal(self):
+        self.hidden = False
+        self.color = "red"
+
+
+@dataclass
+class Number(BoardItem):
+    type: BoardItemTypes = BoardItemTypes.NUMBER
+    bomb_count: int = 5
+
+    def draw(self, screen, outline: bool = False):
+        color = "white" if self.hidden else "green"
+        pygame.draw.rect(screen, color=color, rect=(self.x, self.y, self.width, self.height))
+        if outline:
+            pygame.draw.rect(screen, color=(0, 0, 0), rect=(self.x, self.y, self.width, self.height), width=1)
+
+        #font = pygame.font.SysFont("arial", 24)
+        #img = font.render(str(self.bomb_count), True, "black")
+        #screen.blit(img, (self.x + self.width / 2, self.y + self.height / 2))
+
+    def reveal(self):
+        self.hidden = False
+        self.color = "green"
--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
+import itertools
 import math
 
 import pygame
 
-from button import Button
+from button import Bomb, Number
 import random
 
 WINDOW_HEIGHT = 614
@@ -15,6 +16,7 @@ NUM_OF_BOARD_ITEMS = 64
 OFFSET = round((WINDOW_WIDTH / 2) - (BOARD_WIDTH / 2))
 FRAME_BORDER_COLOR = (128, 128, 128)
 FRAME_BORDER_THICKNESS = 4
+pygame.display.set_caption('PySweeper v0.1')
 
 
 def main():
@@ -22,9 +24,9 @@ def main():
     SCREEN.fill((0, 0, 0))
     game_board = populate_board_array()
     set_board_item_locations(game_board)
-    print(game_board)
+    is_running = True
 
-    while True:
+    while is_running:
         for event in pygame.event.get():
 
             pygame.draw.rect(SCREEN,
@@ -36,38 +38,44 @@ def main():
 
             for board_item in game_board:
                 board_item.draw(SCREEN, outline=True)
-                if board_item.is_over(pygame.mouse.get_pos()):
-                    print("over a button")
 
             if event.type == pygame.QUIT:
-                pygame.quit()
+                is_running = False
             if event.type == pygame.MOUSEBUTTONDOWN:
                 mouse_presses = pygame.mouse.get_pressed()
                 if mouse_presses[0]:
-                    print("Left Mouse key was clicked")
+                    mouse_pos = pygame.mouse.get_pos()
+                    for board_item in game_board:
+                        if board_item.is_over(mouse_pos):
+                            board_item.reveal()
+                            # print("Game over")
+                            # is_running = False  # Set isRunning to False when game over
 
         pygame.display.update()
 
+    pygame.quit()  # Quit Pygame outside the loop
+
 
 def populate_board_array(number_of_bombs: int = 5):
-    # Generate random location for bombs
-    entity_list = [int]
-    for i in range(0, number_of_bombs):
-        n = random.randint(0, NUM_OF_BOARD_ITEMS)
-        entity_list.append(n)
+    bomb_locations: list[tuple[int, int]] = [(x, y) for x, y in
+                                             random.sample([(i, j) for i in range(8) for j in range(8)],
+                                                           number_of_bombs)]
 
-    # Populate board with bombs and normal squares
-    board = []
-    for row in range(NUM_OF_BOARD_ITEMS):
-        if row in entity_list:
-            board.append(Button(color=(255, 0, 0)))
-        else:
-            board.append(Button(color=(255, 255, 255)))
+    board: list[list[Number | Bomb]] = [[Number() for _ in range(8)] for _ in range(8)]
 
-    return board
+    # Calculate neighbouring bombs for each number item
+    # TODO
+
+    for bomb_location in bomb_locations:
+        board[bomb_location[0]][bomb_location[1]] = Bomb()
+
+    # Flatten the list of lists into a single list
+    flat_board_items = list(itertools.chain(*board))
+
+    return flat_board_items
 
 
-def set_board_item_locations(game_board: list[Button]):
+def set_board_item_locations(game_board):
     spacing = 5
     size = BOARD_HEIGHT // math.sqrt(NUM_OF_BOARD_ITEMS)
     for i in range(NUM_OF_BOARD_ITEMS):
